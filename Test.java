4d8b218c08f4ccc6390f5ab6be5bbc74d169e952
irgikaliska <irgikaliska24@gmail.com>
2025-09-30 10:35:01 +0700

    Update Test.java

--- Test.java
+++ Test.java
@@ -5,6 +5,16 @@ public class Test {
         System.out.println("ikan sepat ikan tongkol");
         System.out.println("muka kamu kaya tongkol");
         System.out.println("muka kamu juga");
+
+        int a = 3, b = 4;
+    System.out.println("Total: " + (a + b));
+
+    int x, y, z;
+    x = y = z = 50;
+    System.out.println(x + y + z);
+
+    float myNum = 13.35f;
+    System.out.println(myNum);
         
     }
 }

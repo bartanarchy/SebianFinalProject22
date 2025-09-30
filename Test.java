63308d6e8811e25c88c3c47e327d5cdb7e674f46
Irgi Kaliska <irgikaliska24@gmail.com>
2025-09-30 09:27:46 +0700

    ikan

--- Test.java
+++ Test.java
@@ -3,5 +3,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama saya");
         System.out.println("ikan sepat ikan tongkol");
+        System.out.println("muka kamu kaya tongkol");
+        
     }
 }

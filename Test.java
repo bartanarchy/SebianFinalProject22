6b4fd0f274eea2b4dda1d47298dc355a631ead4b
irgikaliska <irgikaliska24@gmail.com>
2025-09-30 09:17:06 +0700

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ package SebianFinalProject22;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama saya");
+        System.out.println("ikan sepat ikan tongkol")
     }
 }
